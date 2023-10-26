80d126205e4d0b1a86579a4d0aa54f826ff06c4c
Gabriel Fernando Missaka Mendes <gabrielfmm@al.insper.edu.br>
2023-10-26 09:31:27 -0300

    Funcionalidade de ler QR Code e testar acesso implementada

--- admin.py
+++ admin.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
-from PIL import Image
-from datetime import time
+import json
+import cv2
 
 # Defina um ícone personalizado para a página
 st.set_page_config(
@@ -11,7 +11,6 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-
 # Estilize o título da página
 st.title("Bem-vindo ao Painel de Administração")
 
@@ -20,59 +19,182 @@ menu = st.sidebar.selectbox("Selecione uma opção:", ["Cadastrar Usuário", "Ca
 
 # Aba "Cadastrar Usuário"
 if menu == "Cadastrar Usuário":
+    # Título da aba
     st.header("Cadastro de Usuário")
 
-    # Campos de entrada para o login e o nome do usuário
-    login_usuario = st.text_input("Login de Usuário")
-    nome_usuario = st.text_input("Nome de Usuário")
+    # Campo de entrada para o login e nome de usuário
+    login_usuario = st.text_input("Login de usuário:")
+    nome_usuario = st.text_input("Nome de usuário:")
 
-    # Campo de entrada para o nível de permissão do usuário
-    nivel_usuario = st.selectbox("Nível de Permissão", [1, 2, 3, 4, 5])
+    # Campo de entrada para o nível de permisão de usuário
+    nivel_usuario = st.selectbox("Nível de permissão de usuário:", [1, 2, 3, 4, 5])
 
-    # Botão com estilo personalizado
+    # Botão para cadastrar usuário
     if st.button("Cadastrar Usuário", key="cadastro_usuario"):
-        if login_usuario and nome_usuario:
-            # Realize a requisição para o backend
-            
-            response = requests.post(f"http://127.0.0.1:5000/usuario", json={"login": login_usuario, "nome": nome_usuario, "permissao": nivel_usuario})
-            if response.status_code == 201:
-                st.success("Usuário cadastrado com sucesso.")
-            elif response.status_code == 400:
-                st.warning("Usuário já cadastrado.")
+        # Se o login de usuário for infomado, então:
+        if login_usuario:
+            # Se o nome de usuário for infomado, então:
+            if nome_usuario:
+                # Realiza a requisição para o backend
+                response = requests.post(f"http://127.0.0.1:5000/usuario", json={"login": login_usuario, "nome": nome_usuario, "permissao": nivel_usuario})
+
+                # Se a resposta for 201, então:
+                if response.status_code == 201:
+                    st.success("Usuário cadastrado com sucesso.")
+
+                # Se a resposta for 400, então:
+                elif response.status_code == 400:
+                    st.warning("Usuário já cadastrado.")
+
+                # Se a resposta for diferente de 201 e 400, então:
+                else:
+                    st.error("Erro ao cadastrar usuário. Tente novamente mais tarde.")
+
+            # Se o nome de usuário não for informado, então:
             else:
-                st.error("Erro ao cadastrar usuário. Tente novamente mais tarde.")
+                st.warning("Por favor, insira um nome de usuário para cadastrar.")
+
+        # Se o login de usuário não for informado, então:
         else:
-            st.warning("Por favor, preencha todos os campos.")
+            st.warning("Por favor, insira um login de usuário para cadastrar.")
 
 # Aba "Cadastrar Porta"
 if menu == "Cadastrar Porta":
+    # Título da aba
     st.header("Cadastro de Porta")
 
     # Campos de entrada para o número do prédio e da sala
-    numero_predio = st.text_input("Número do Prédio")
-    numero_sala = st.text_input("Número da Sala")
+    numero_predio = st.text_input("Número do prédio:")
+    numero_sala = st.text_input("Número da sala:")
 
-    # Campo de entrada para o nível de permissão de acesso
-    nivel_acesso = st.selectbox("Nível de Permissão de Acesso", [1, 2, 3, 4, 5])
+    # Campo de entrada para o nível de permisão de acesso
+    nivel_acesso = st.selectbox("Nível de permissão de acesso:", [1, 2, 3, 4, 5])
 
-    # Campo de entrada para exceções de acesso (opcional)
-    excecoes_acesso = st.text_input("Exceções de Acesso (opcional)")
+    # Campo de entrada para escrever as exceções de acesso (opcional)
+    excecoes_acesso = st.text_input("Exceções de acesso (opcional):")
+
+    # Convertendo as exceções de acesso para lista
+    excecoes_acesso = excecoes_acesso.split(", ")
 
     # Botão com estilo personalizado
     if st.button("Cadastrar Porta", key="cadastro_porta"):
-        if numero_predio and numero_sala:
-            # Realize a requisição para o backend
-            
-            response = requests.post(f"http://127.0.0.1:5000/porta", json={"predio": int(numero_predio), "sala": int(numero_sala), "permissao": nivel_acesso, "excecoes": excecoes_acesso.split(", ")})
-            
-            if response.status_code == 201:
-                st.success("Porta cadastrada com sucesso.")
-            elif response.status_code == 400:
-                st.warning("Porta já cadastrada.")
+        # Se o número do prédio for infomado, então:
+        if numero_predio:
+            # Se o número da sala for infomado, então:
+            if numero_sala:
+                # Realiza a requisição para o backend
+                response = requests.post(f"http://127.0.0.1:5000/porta", json={"predio": int(numero_predio), "sala": int(numero_sala), "permissao": nivel_acesso, "excecoes": excecoes_acesso})
+
+                # Se a resposta for 201, então:
+                if response.status_code == 201:
+                    st.success("Porta cadastrada com sucesso.")
+
+                # Se a resposta for 400, então:
+                elif response.status_code == 400:
+                    st.warning("Porta já cadastrada.")
+
+                # Se a resposta for diferente de 201 e 400, então:
+                else:
+                    st.error("Erro ao cadastrar porta. Tente novamente mais tarde.")
+
+            # Se o número da sala não for informado, então:
             else:
-                st.error("Erro ao cadastrar porta. Tente novamente mais tarde.")
+                st.warning("Por favor, insira um número da sala para cadastrar.")
+
+        # Se o número do prédio não for informado, então:
         else:
-            st.warning("Por favor, preencha todos os campos.")
+            st.warning("Por favor, insira um número do prédio para cadastrar.")
+
+# Aba "Testar Acesso"
+if menu == "Testar Acesso":
+    # Título da aba
+    st.header("Testar Acesso")
+
+    # Campo de entrada para o número do prédio
+    numero_predio = st.text_input("Digite o número do prédio:")
+
+    # Campo de entrada para o número da sala
+    numero_sala = st.text_input("Digite o número da sala:")
+
+    # Botão com estilo personalizado
+    if st.button("Testar Acesso", key="testar_acesso"):
+        # Se o número do prédio for informado, então:
+        if numero_predio:
+            # Se o número da sala for infomado, então:
+            if numero_sala:
+                # Variável para armazenar os dados do QR Code
+                qr_code_data = None
+                
+                # Abre a câmera
+                cap = cv2.VideoCapture(0)
+
+                # Loop para capturar o frame da câmera
+                while True:
+                    # Captura o frame da câmera
+                    ret, frame = cap.read()
+                    
+                    # Se não conseguir acessar câmera, então:
+                    if not ret:
+                        st.write("Erro ao acessar a câmera.")
+                        break
+                    
+                    # Instancia o detector de QR Code
+                    detector = cv2.QRCodeDetector()
+
+                    # Detecta Code
+                    data, vertices, _ = detector.detectAndDecode(frame)
+
+                    # Se os dados do QR Code forem detectados, então:
+                    if data:
+                        st.write("Código QR detectado:")
+                        st.image(frame, channels="BGR", use_column_width=True)
+
+                        # Tenta decodificar o JSON do QR Code
+                        try:
+                            # Recebe os dados do QR Code
+                            qr_code_data = json.loads(data)
+
+                            # Recebe login do usuário e armazena na variável
+                            login_usuario = qr_code_data.get("login", None)
+                        
+                        # Se não conseguir decodificar o JSON do QR Code, então:
+                        except json.JSONDecodeError:
+                            st.warning("Erro ao decodificar o JSON do QR Code.")
+                            qr_code_data = None
+                            login_usuario = None
+                        break
+                
+                # Fecha a câmera
+                cap.release()
+
+                # Se o login do usuário for informado, então:
+                if login_usuario:
+                    # Realiza a requisição para o backend
+                    response = requests.get(f"http://127.0.0.1:5000/acesso/usuario/{login_usuario}/predio/{numero_predio}/sala/{numero_sala}")
+
+                    # Se a resposta for 200, então:
+                    if response.status_code == 200:
+                        st.success("Acesso permitido.")
+
+                    # Se a resposta for 403, então:
+                    elif response.status_code == 403:
+                        st.warning("Acesso negado.")
+
+                    # Se a resposta for diferente de 200 e 403, então:
+                    else:
+                        st.warning("Porta não encontrada.")
+                
+                # Se o login do usuário não foi detectado, então:
+                else:
+                    st.warning("Por favor, insira um QR Code válido.")
+            
+            # Se o número da sala não for informado, então:
+            else:
+                st.warning("Por favor, insira um número da sala para testar acesso.")
+        
+        # Se o número do prédio não for informado, então:
+        else:
+            st.warning("Por favor, insira um número do prédio para testar acesso.")
 
 # Divisor para separar seções
 st.write("---")
@@ -82,4 +204,3 @@ st.markdown("Desenvolvido por [Seu Nome]")
 st.markdown("[GitHub](https://github.com/seu-usuario) | [LinkedIn](https://www.linkedin.com/in/seu-usuario)")
 
 # Adicione cores de fundo e estilização de texto conforme necessário para destacar informações importantes.
-
